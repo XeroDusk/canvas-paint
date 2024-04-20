ed97cbaf89b7b93d63bbbcf069124bc0df37f831
Dusk <asutoshpradhan424@gmail.com>
2024-04-21 00:46:05 +0530

    Add slider to change brush width

--- script.js
+++ script.js
@@ -6,26 +6,31 @@ canvas.style.border = "solid black 2px";
 
 
 
+
 const mouse = 0;
 function mousedown(e) {
     if (e.button === mouse) {
-      shouldDraw = true;    
-      ctx.beginPath();    
-      ctx.moveTo(e.clientX , e.clientY );
+        shouldDraw = true;
+        let walls = e.target.getBoundingClientRect();
+        ctx.beginPath();  
+        ctx.moveTo(e.clientX - walls.left, e.clientY - walls.top);
     }
 }
 function mouseup(e) {
     if (e.button === mouse) {
-      shouldDraw = false;
+        shouldDraw = false;
     }
 }
 function mousemove(e) {
     if (shouldDraw) {
-
-      ctx.lineTo(e.clientX, e.clientY);
-      ctx.stroke();
+        let walls = e.target.getBoundingClientRect();
+        ctx.lineTo(e.clientX-walls.left, e.clientY-walls.top);
+        ctx.stroke();
     }
 }
 canvas.addEventListener('mousedown', mousedown); 
 canvas.addEventListener('mouseup', mouseup);
 canvas.addEventListener('mousemove', mousemove);
+
+const tip = document.querySelector('#tip');
+tip.addEventListener("change", () => ctx.lineWidth = tip.value);
