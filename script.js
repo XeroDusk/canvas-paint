00a5c50189209f9000d8536aa93e43803ba7a395
Dusk <asutoshpradhan424@gmail.com>
2024-04-21 00:13:37 +0530

    Add drawing from mouse functionality to canvas

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-canvas.height = 700;
 canvas.width = 700;
+canvas.height = 700;
 canvas.style.border = "solid black 2px";
 
-var info = document.getElementById("info");
-function tellPos(p){
-    info.innerHTML = "x: " + p.pageX + " y: " + p.pageY; 
+
+
+const mouse = 0;
+function mousedown(e) {
+    if (e.button === mouse) {
+      shouldDraw = true;    
+      ctx.beginPath();    
+      ctx.moveTo(e.clientX , e.clientY );
+    }
 }
-
-function getPos(p){
-    return [p.pageX, p.pageY];
+function mouseup(e) {
+    if (e.button === mouse) {
+      shouldDraw = false;
+    }
 }
+function mousemove(e) {
+    if (shouldDraw) {
 
-
-
-function dot(p){
-    let x = p.pageX; let y = p.pageY;
-    ctx.beginPath();
-    ctx.arc(x,y,5,0,2*Math.PI);
-    ctx.fill();
-    ctx.stroke();
+      ctx.lineTo(e.clientX, e.clientY);
+      ctx.stroke();
+    }
 }
-
-function draw(p){
-    let x = p.pageX; let y= p.pagey;
-    addEventListener('mouseup', ()=> {ctx.moveTo(x,y);ctx.lineTo(p.pageX,p.pageY);ctx.stroke();}, false);
-}
-
-addEventListener('mousemove', tellPos, false);
-addEventListener('mousedown', dot, false);
-
-addEventListener('mousedown', draw, false);
+canvas.addEventListener('mousedown', mousedown); 
+canvas.addEventListener('mouseup', mouseup);
+canvas.addEventListener('mousemove', mousemove);
